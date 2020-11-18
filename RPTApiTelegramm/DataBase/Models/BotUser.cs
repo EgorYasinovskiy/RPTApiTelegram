d50f2da46af70b5a12bdc3f05c727be468aface1
Egor Yasinovskiy <Yasinovskiy.E@digdes.com>
2020-11-18 20:40:12 +0300

    Добавил пакет Telegram.Bot Api

--- RPTApiTelegramm/DataBase/Models/BotUser.cs
+++ RPTApiTelegramm/DataBase/Models/BotUser.cs
@@ -6,8 +6,7 @@ namespace RPTApiTelegram.DataBase.Models
 {
     public class BotUser
     {
+        public int Id { get; set; } // Inherits from TelegramUser.
         public virtual List<Order> Orders { get; set; }
-        public 
-
     }
 }
