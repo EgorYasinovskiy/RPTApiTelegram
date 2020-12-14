94aa593eaff8d8afa5778863dd2322aae202f6d3
Egor Yasinovskiy <Yasinovskiy.E@digdes.com>
2020-12-14 19:38:35 +0300

    Добавил и реализовал клавиатуры

---
+++ RPTApiTelegramm/Telegram/Keyboards/CommandsKeyboard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace RPTApi.Telegram.Keyboards
+{
+    public static class CommandsKeyboard
+    {
+        private static global::Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup ReplyKeyboardMarkup = new global::Telegram.Bot.Types.ReplyMarkups.ReplyKeyboardMarkup()
+        {
+            Keyboard = new List<List<global::Telegram.Bot.Types.ReplyMarkups.KeyboardButton>>()
+            {
+                new List<global::Telegram.Bot.Types.ReplyMarkups.KeyboardButton>()
+                {
+                    new global::Telegram.Bot.Types.ReplyMarkups.KeyboardButton()
+                    {
+                        Text = "/track"
+                    },
+                    new global::Telegram.Bot.Types.ReplyMarkups.KeyboardButton()
+                    {
+                        Text = "/rename"
+                    },
+                    new global::Telegram.Bot.Types.ReplyMarkups.KeyboardButton()
+                    {
+                        Text = "/remove"
+                    }
+
+                }
+            },
+            ResizeKeyboard = true
+        };
+        public static global::Telegram.Bot.Types.ReplyMarkups.IReplyMarkup Get()
+        {
+            return ReplyKeyboardMarkup;
+        }
+      
+    }
+}
---
+++ RPTApiTelegramm/Telegram/Keyboards/TrackReplyKeyboard.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using global::Telegram.Bot.Types.ReplyMarkups;
+
+namespace RPTApi.Telegram.Keyboards
+{
+    public static class TrackReplyKeyboard
+    {
+        public static IReplyMarkup Get(int userId, DataBase.BotDataContext dataBase)
+        {
+            List<InlineKeyboardButton> tracks = new List<InlineKeyboardButton>();
+            foreach(var order in dataBase.Users.FirstOrDefault(u => u.Id == userId).Orders)
+            {
+                tracks.Add(new InlineKeyboardButton() { Text=$"{order.Name ?? order.Barcode}",CallbackData=$"/track {order.Name ?? order.Barcode}"});
+            }
+            return new InlineKeyboardMarkup(tracks);
+        }
+    }
+}
